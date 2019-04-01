27c8edf7b340a01d903ac7e0496943eea161e2f5
Tedu <lvze@tedu.cn>
2019-04-01 11:21:58 +0800

    change

--- file_send.py
+++ file_send.py
@@ -1,16 +1,1 @@
-from socket import *
-#设置套接字
-s=socket()
-# 建立连接
-s.connect(("127.0.0.1",8888))
-
-#读取文件
-f=open("hua.jpg","rb")
-while True:
-    data=f.read(1024)
-    if not data:
-        break
-    s.send(data)
-
-f.close()
-s.close()
+print("hello")
